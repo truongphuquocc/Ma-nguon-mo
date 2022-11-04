8ff15ca08cd8cef96f73e1d118d05921f004bc93
PhuQuoc <tpquoc22@gmail.com>
2022-11-04 15:01:32 +0700

    form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
 
 from app import routes
-app.config["SECRET_KEY"] = "hard-secret-key"
+#app.config["SECRET_KEY"] = "hard-secret-key"
+app.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 # from crypt import methods
 from turtle import title
 from app import app
-from flask import render_template, redirect,flash
+from flask import render_template, redirect, flash
 from app.forms import LoginForm
+
 
 @app.route("/")
 @app.route("/index")
@@ -24,12 +25,13 @@ def index():
         {"name": "Quoc", "java": 8, "php": 9, "C": 8}
     ]
     login_form = LoginForm()
-    return render_template("index.html", user = user_info, title = title, students= students_info, form = login_form)
+    return render_template("index.html", user=user_info, title=title, students=students_info, form=login_form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        flash(f"Uername: {login_form.username.data}")
+        flash(f"Username: {login_form.username.data}")
         return redirect("index")
-    return render_template("login.html", form = login_form)
+    return render_template("login.html", form=login_form)
