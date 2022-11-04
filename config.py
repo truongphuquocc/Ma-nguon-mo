38cfd21684168d0cb52125d3cde5c944cfef8dbc
PhuQuoc <tpquoc22@gmail.com>
2022-11-04 15:30:09 +0700

    config sqlite

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SECRET_KEY = "your-hard-secret-key"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "your-hard-secret-key"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
