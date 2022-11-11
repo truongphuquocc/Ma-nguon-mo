b3461249bccec77dd7f6e5a49932d699627f6270
PhuQuoc <tpquoc22@gmail.com>
2022-11-11 15:43:04 +0700

    create table SQL lite

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,14 @@ from config import Config
 app = Flask(__name__)
 
 from app import routes
-#app.config["SECRET_KEY"] = "hard-secret-key"
 app.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+app.app_context().push()
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import models
+
