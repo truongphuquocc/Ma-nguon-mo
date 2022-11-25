197c3efec5ee96b3bdcc39e25cf777f5d33e9b80
PhuQuoc <tpquoc22@gmail.com>
2022-11-25 12:58:06 +0700

    create table post

--- app/models.py
+++ app/models.py
@@ -1,9 +1,21 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(64))
+    posts = db.relationship("Post", backref="author", lazy=True)
 
     def __repr__(self):
         return f"Username is: {self.username}"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"Post content is: {self.content}"
+        
